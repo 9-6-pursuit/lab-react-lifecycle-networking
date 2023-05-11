a4137ae11178a6a02e3cbd8401693d7483710e8f
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-05-11 12:44:17 -0400

    finished react lifecycle networking lab

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,13 +1,44 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
+
 export const EmployeeList = () => {
+
+  const [employees, setEmployees] = useState([])
+
+  useEffect(()=> {
+    fetch(`https://resource-veterinarian-api.fly.dev/api/employees`)
+    .then((response) => response.json())
+    .then((response) => {
+   
+      setEmployees(response)
+    })
+    .catch((error) => console.error(error))
+  }, [])
+
+  // BASE_URL = https://resource-veterinarian-api.fly.dev/api
+  // Get all employees: `${BASE_URL}/employees`
+  // Get a single employee: `${BASE_URL}/employees/${employee.id}`
+  // Get all pets: `${BASE_URL}/api/pets`
+  // Get all pets assigned to an employee: `${BASE_URL}/api/pets?employeeId=${employee.id}`
+
+
+
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {employees.map(employee => {
+          return (
+            <Employee 
+            key = {employee.id}
+            employee = {employee}
+            />
+            )
+          })}
       </section>
     </main>
   );
