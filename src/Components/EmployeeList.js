b6858198b97849713beae27a18be907debb228e9
crsvalle <cristian.valle@pursuit.org>
2023-05-10 22:23:48 -0400

    lifecycle

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
 export const EmployeeList = () => {
+  
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(()=>{
+    
+    fetch('https://resource-veterinarian-api.fly.dev/api/employees')
+    .then((response) =>response.json())
+    .then((response) => {
+      setEmployees(response)
+    })
+  }, [])
+
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {employees.map((employee) => {
+          return(
+            <Employee employee={employee} key={employee.id}/>
+          )
+        })}
       </section>
     </main>
   );
