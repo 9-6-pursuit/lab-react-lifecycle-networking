bf02c8f002f28632f482c518b163091df764d808
JenniferEinstein <jennifereinstein@gmail.com>
2023-05-12 20:48:05 -0400

    Caught up to the end of Wednesday's class.

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,15 +1,53 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { useEffect, useState } from "react"
 
-export const Employee = () => {
+export const Employee = ({ employee }) => {
+  const [ pets, setPets ] = useState([]);
+  const [ showPets, setShowPets ] = useState(false)
+  
+
+  useEffect( () => {
+    fetch(`https://resource-veterinarian-api.fly.dev/api/pets?employeeId=${employee.id}`)
+    .then((response) => response.json())
+    .then((response) => { 
+      console.log("useEffect Employee.js")
+      setPets(response)
+    })
+  }, [employee.id])
+
+const toggleShowPets = () => {
+  setShowPets(!showPets);
+
+}
+
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+      <h3>{employee.prefix} {employee.firstName} {employee.lastName} </h3>
+      <h4>{employee.title}</h4>
+      {showPets ? (
+        <>
+          <button onClick={toggleShowPets}>Hide Pets</button> 
+          <PetList pets={pets}/> 
+        </>
+      ) : (
+        <button onClick={toggleShowPets}>Show Pets</button>
+      )}
+
+   
     </article>
   );
 };
 
 export default Employee;
+
+
+  // function getPets() {
+  //   console.log("Get the pets")
+  //   pets.map(pet => {
+  //     return pet.name
+  //   }).join(", ")
+  // } 
+
+  //  pet.name + ", " +
+  //  let petListForOne = []
--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,13 +1,33 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
 export const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]);
+  
+  // let BASE_URL = "https://resource-veterinarian-api.fly.dev/api"
+  // I tried doing something with BASE_URL, but it didn't work out
+
+
+  useEffect(() => {
+    fetch(`https://resource-veterinarian-api.fly.dev/api/employees`)
+      .then((response) => response.json())
+      .then((response)=> {
+        console.log("The response is ", response)
+        setEmployees(response);
+      })
+  }, [])
+
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {employees.map(employee => {
+          return (
+            <Employee key={employee.id} employee={employee}/>
+          )
+        })}
       </section>
     </main>
   );
