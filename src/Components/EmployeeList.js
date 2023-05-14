64bb5796a16354086c0dc9e1bcf425f6583585ac
AllahvelS <allahvelsalisbury@pursuit.org>
2023-05-13 22:04:40 -0400

    Lifecycle.

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,13 +1,36 @@
 import React from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
+import { useEffect, useState } from "react";
+
 
 export const EmployeeList = () => {
+  const [employees, setEmployees] = useState([])
+
+
+useEffect(() => {
+  fetch("https://resource-veterinarian-api.fly.dev/api/employees")
+  .then((response) => response.json())
+  .then((response) => {
+    console.log("it works")
+    console.log(response)
+    setEmployees(response)
+  })
+  //second  useEffect parameter ([]) prevents above code from going into infinite loop
+}, [])
+
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {/* for each employee on list, create one employee component */}
+        {employees.map(employee => {
+          return (
+
+            <Employee key={employee.id}
+            employee={employee} />
+          )
+        })}
       </section>
     </main>
   );
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,7 +1,14 @@
-export const PetList = () => {
+export const PetList = ({pets}) => {
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {/* <p>No pets listed for this employee.</p> */}
+      {pets.length > 0 ? 
+      <p>{pets.map(pet => {
+        return pet.name 
+      }).join(", ")}
+      </p> :
+      <p>No Pets Listed</p>
+    }
     </aside>
   );
 };
