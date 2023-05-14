e8f83842758fc18c853cc4f6ecd2f6b55f69dba3
Sabri Mohiuddin <you@example.com>
2023-05-14 05:15:01 -0400

    lab-react-lifecycle-networking

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,15 +1,44 @@
 import PetList from "./PetList";
+import { useEffect, useState } from 'react';
 import "./Employee.css";
 
-export const Employee = () => {
+const BASE_URL = `https://resource-veterinarian-api.fly.dev/api`
+
+export const Employee = ({employee}) => {
+  const [showPets, setShowPets] = useState(false);
+  const [pets, setPets] = useState ([])
+
+  useEffect(() => {
+    fetch (`${BASE_URL}/api/pets?employeeId=${employee.id}`)
+      .then ((response) => response.json())
+      .then ((response) => {
+        setPets(response)
+      })
+
+  },  [employee.id])
+  
+  const toggleShowPets = () => {
+    setShowPets(!showPets)
+  }
+
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
-    </article>
+      <h3>{employee.prefix ? employee.prefix + " " : ""}{employee.firstName} {employee.lastName}{employee.postfix ? ", " + employee.postfix : ""}</h3>
+      <h4>{employee.tittle}</h4>
+      {showPets ? (
+        <>
+          <button onClick={toggleShowPets}>Hide Pets</button>
+          <PetList pets={pets} />
+        </>
+        ) : (  
+        <button onClick={toggleShowPets}>Show Pets</button> 
+      )}
+    </article>    
   );
 };
 
 export default Employee;
+
+
+
+
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -9,3 +9,5 @@ export const NavBar = () => {
 };
 
 export default NavBar;
+
+
