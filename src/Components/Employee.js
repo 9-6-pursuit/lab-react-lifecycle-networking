55f729aef2914c9f711f3fd3a772df1f9c128048
Phillip Mitchell <4kphil.@Phillips-MacBook-Air.local>
2023-05-14 09:51:30 -0400

    have a error when running my local page says useEffect is already declared

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,15 +1,37 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { useEffect, useState } from "react";
 
-export const Employee = () => {
+export const Employee = ({ employee }) => {
+  const [showPets, setShowPets] = useState(false);
+  const [pets, setPets] = useState([]);
+
+
+  useEffect(() => {
+    fetch(`https://resource-veterinarian-api.fly.dev/api/pets?employeeId=${employee.id}`)
+    .then(response => response.json())
+    .then(petsData => setPets(petsData))
+  },[employee.id]);
+
+  const toggleShowPets = () => {
+    setShowPets(!showPets);
+  };
+
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+    <h3>
+      {employee.prefix ? `${employee.prefix} ` : ""} {employee.firstName}  {employee.lastName} {employee.postfix ? `, ${employee.postfix}` : ""}
+      </h3>
+      <h4>{employee.title}</h4>
+      {showPets ? (
+        <>
+          <button onClick={toggleShowPets}>Show Pets</button>
+          <PetList pets={pets} />
+        </>
+      ) : (
+        <button onClick={toggleShowPets}>Show Pets</button>
+      )}
     </article>
   );
 };
-
 export default Employee;
